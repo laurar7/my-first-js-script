8d752f8834918460e08b8b952a2f486e207a92b0
Laura R <102334514+laurar7@users.noreply.github.com>
2022-07-30 13:49:39 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Write your JavaScript below this line
+var name = "Laura";
+var favoriteSnack = "Bananas";
+var numberOfHorses = "8";
+var favoriteFlower = "rose";
+var cityBorn = "Santo Domingo";
+var birthday = "July 7th";
+
+console.log(
+  "Hi, my name is " +
+    name +
+    " and I wish I had " +
+    favoriteSnack +
+    " right now. "
+);
+
+console.log(numberOfHorses);
+
+console.log("My favorite flower is " + favoriteFlower + ".");
+
+console.log(
+  "Hi, my name is " +
+    name +
+    " and I was born in " +
+    cityBorn +
+    " on " +
+    birthday +
+    "."
+);
